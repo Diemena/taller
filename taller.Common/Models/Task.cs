d6ff981d355d1cee9b63f038c2eed5c2ee2dab60
Diemena <47550029+Diemena@users.noreply.github.com>
2021-09-06 00:52:05 -0500

    Add entity

---
+++ taller.Common/Models/Task.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace taller.Common.Models
+{
+    class Task
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
